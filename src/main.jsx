774b76a06c98fee4b986389c8adadc00524d8970
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-03-23 02:08:57 +0330

    Feat:connet qraphql to react project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import ReactDOM from 'react-dom/client'
+//components
 import App from './App.jsx'
+//styles
 import './index.css'
 
+const client = new ApolloClient({
+  uri:"https://api-us-west-2.hygraph.com/v2/cl5ex4xej1o9h01ug1jevguyc/master",
+  cache:new InMemoryCache()
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
