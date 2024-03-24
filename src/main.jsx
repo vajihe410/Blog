859831acb411a14403f3055ec5dd01546acb9fea
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-03-24 12:39:28 +0330

    Feat: use .env

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import './index.css'
 import '../src/styles/font.css'
 
 const client = new ApolloClient({
-  uri:"https://api-us-west-2.hygraph.com/v2/cl5ex4xej1o9h01ug1jevguyc/master",
+  uri:process.env.REACT_APP_GRAPHCMS_URI,
   cache:new InMemoryCache()
 })
 ReactDOM.createRoot(document.getElementById('root')).render(
