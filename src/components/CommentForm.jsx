5874d307d5bd9f0dc0c6df796ca5b57ebe5d2a37
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2024-04-01 14:39:01 +0330

    Feat:mutation and send comment

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,12 +1,33 @@
 import React, { useState } from 'react'
 import { Button, Grid, TextField, Typography } from '@mui/material'
+import { useMutation } from '@apollo/client'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+//mutations
+import { SEND_COMMENT } from '../graphQL/mutations'
 
 function CommentForm({slug}) {
 
     const [name,setName] = useState("")
     const [email,setEmail] = useState("")
-    const [comment,setComment] = useState("")
+    const [text,setText] = useState("")
 
+    const [sendComment,{loading,data,errors} ] = useMutation(SEND_COMMENT,{variables:{name,email,text,slug}})
+    const sendHandler = () => {
+      if(name && email && text){
+        sendComment()
+      }
+      else{
+        toast.warn(' فیلدهارا پر کن', {
+          position: "top-center",
+          });
+      }
+    }
+    if(data){
+      toast.success(' نظر شما با موفقیت ارسال شد', {
+        position: "top-center",
+        });
+    }
   return (
     <Grid container sx={{boxShadow:"rgba(0,0,0,0.1) 0px 4px 12px", borderRadius:4 , py:1 ,mt:5}}>
         <Grid item xs={12} m={2}>
@@ -19,10 +40,12 @@ function CommentForm({slug}) {
                 <TextField  label='ایمیل' variant='outlined' value={email} onChange={(e)=>setEmail(e.target.value)} sx={{width:'100%'}}/>
         </Grid>
         <Grid item xs={12} m={2}> 
-                <TextField  label='متن کامنت' variant='outlined' value={comment} multiline minRows={4} onChange={(e)=>setComment(e.target.value)} sx={{width:'100%'}}/>
+                <TextField  label='متن کامنت' variant='outlined' value={text} multiline minRows={4} onChange={(e)=>setText(e.target.value)} sx={{width:'100%'}}/>
         </Grid>
         <Grid item xs={12} m={2}>
-            <Button variant='contained'>ارسال</Button>
+          {loading ? <Button variant='contained' disabled onClick={sendHandler}>در حال ارسال</Button> : <Button variant='contained' onClick={sendHandler}>ارسال</Button>}
+            
+            <ToastContainer />
         </Grid>
     </Grid>
   )
